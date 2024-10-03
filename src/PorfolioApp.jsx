9fff7739bfd1216cd2b4e9c4ced2b2d300ce5736
Carolina Case <69168196+carolinacase@users.noreply.github.com>
2024-10-03 17:47:19 -0300

    Initial commit

---
+++ src/PorfolioApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class PorfolioApp extends Component {
+  render() {
+    return (
+      <div>PorfolioApp</div>
+    )
+  }
+}
+
+export default PorfolioApp
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import  PorfolioApp  from './PorfolioApp'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <PorfolioApp/>
+  </StrictMode>,
+)
